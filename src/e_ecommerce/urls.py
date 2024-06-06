1fb21f335304c2049b97415f3e5c08adc73e22b7
Agatha <agatha.cristina0507@gmail.com>
2024-06-06 17:45:19 -0300

    create context variable title and context to home_page, about_page and contact_page, create and set src/template/about/views.html, src/template/contact/view.html and update src/template/home page.html to use context variables

--- src/e_ecommerce/urls.py
+++ src/e_ecommerce/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from .views import home_page, about_page, contact_page
 urlpatterns = [
     path('', home_page),
-    path('admin/', admin.site.urls),
+    path('about/', about_page),
     path('contact/', contact_page),
     path('admin/', admin.site.urls),
 ]
