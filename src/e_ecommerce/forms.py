63430ffdb44b7a19932383ad5f387820db9f8439
Agatha <agatha.cristina0507@gmail.com>
2024-06-09 21:12:48 -0300

    e_commerce/views.py and template/contact/view.html was modifield

--- src/e_ecommerce/forms.py
+++ src/e_ecommerce/forms.py
@@ -1,5 +1,35 @@
 from django import forms
-class ContractForms(forms.Forms):
-    fullname= forms.CharField()
-    email = forms.EmailField()
-    content = forms.CharField()
+
+class ContactForm(forms.Form):
+    full_name = forms.CharField(
+        error_messages={'invalid': 'É obrigatorio o preenchimento do nome!'},
+        widget=forms.TextInput(
+            attrs={
+                    "class": "form-control", 
+                    "placeholder": "Seu nome completo"
+                }
+            )
+            )
+    email = forms.EmailField(
+        error_messages={'invalid': 'Digite um email válido'},
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+            "placeholder": "Digite seu email"
+            }
+        )
+    )
+    content = forms.CharField(
+        error_messages={'invalid': 'É obrigatorio o preenchimento do campo mensagem!'},
+            widget=forms.Textarea(
+            attrs={
+                    "class": "form-control", 
+                    "placeholder": "Digite sua mensagem"
+                }
+            )
+    )
+    def clean_mail(self):
+        email= self.cleaned_data.get("Email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("O email deve ser do gmail.com")
+        return email
